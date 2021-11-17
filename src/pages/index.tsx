2d919f79dab59355a7bc3db1b7e4447e8c0e73f2
Brice Duke <bricewduke@gmail.com>
2021-11-16 23:02:13 -0600

    feat: set up web3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,36 @@
-import { Container } from '@chakra-ui/react';
-import React, { ReactElement, useEffect } from 'react';
+import { Button, Container } from '@chakra-ui/react';
+import React, { ReactElement, useEffect, useState } from 'react';
+
+import { getBalance, getWallet } from '../../lib/Client';
 
 interface Props {}
 
 export default function Index({}: Props): ReactElement {
-	useEffect(() => {}, []);
+	const [balance, setBalance] = useState(0);
+	const [walletAddress, setWalletAddress] = useState('');
 
-	return <Container height="100vh"></Container>;
+	const fetchBalance = async () => {
+		const bal = await getBalance().catch((err) => console.error(err));
+
+		setBalance(bal);
+	};
+
+	const fetchWalletAddress = async () => {
+		const wallet = await getWallet();
+
+		setWalletAddress(wallet);
+	};
+
+	useEffect(() => {
+		fetchBalance();
+		fetchWalletAddress();
+	}, []);
+
+	return (
+		<Container height="100vh">
+			<h1>Your balance is {balance}</h1>
+			<h1>Wallet address: {walletAddress}</h1>
+			<Button onClick={fetchBalance}>Refresh</Button>
+		</Container>
+	);
 }
