f2e2ddf5a09cc9dca40d2ff77cdd5e57ee2c4c68
Brice Duke <bricewduke@gmail.com>
2021-11-16 21:58:02 -0600

    feat: initialize app

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@chakra-ui/react';
+import React, { ReactElement, useEffect } from 'react';
+
+interface Props {}
+
+export default function Index({}: Props): ReactElement {
+	useEffect(() => {}, []);
+
+	return <Container height="100vh"></Container>;
+}
